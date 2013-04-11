41230b6791720c5d0fb2e7e77e2f9c4baaf47701
Michele Schimd <michele.schimd@gmail.com>
2013-04-11 16:50:05 +0200

    Still GAE model development: GAEStop, GAETimetables

--- src/gaemodel/stops.py
+++ src/gaemodel/stops.py
@@ -10,12 +10,21 @@ import position as gaepos
 import model.position as modelpos
 from model import stops
 
-# TODO: implement getGAEStopById function
+def getGAEStopByModelId(mid):
+    """Return a GAEStop with specified mid"""
+    result = GAEStop.query(GAEStop.mid == mid).fetch()
+    # zero length means no entity with specified mid, return None
+    if len(result) <= 0:
+        result = [None]
+    # since twice the same id should not be allowed in 
+    # such situation we can return the only available results
+    return result[0]
 
 def getGAEStop(stop):
+    """Return a GaeStop created from the given Stop model class"""
     return GAEStop(mid=stop.id, position=ndb.GeoPt(stop.position.latitude,
                                                    stop.position.longitude)) 
-
+###### GAEStop class ######
 
 class GAEStop(gaepos.GAEGeoPositionedItem):
     def toStop(self):
--- src/gaemodel/timetables.py
+++ src/gaemodel/timetables.py
@@ -4,20 +4,27 @@ Created on Apr 8, 2013
 @author: Michele Schimd
 '''
 
+import datetime
+
 from google.appengine.ext.ndb import polymodel
 from google.appengine.ext import ndb
 
+from gaemodel import stops
+import model.timetables as mtt
 
-# TODO: Add conversion functions (needs getGAEStopById() )
+
+# TODO (Performance): on conversion function us bulk of stops retrieves from the datastore 
 
 def getGAETimetable(timetable):
     gaetable = []
     modeltable = timetable.table
     for item in modeltable:
-        gaetable.append(TimetableItem(time=item[0].seconds, stop=item[1]))
+        gaestop = stops.getGAEStopByModelId(item[1].id)
+        gaetable.append(TimetableItem(time=item[0].seconds, stop=gaestop.key))
         
-    return GAETimetables(initstop=timetable.initial[0],
-                         inittime=timetable.initial[1],
+    initialStopKey = stops.getGAEStopByModelId(timetable.initial[1].id).key
+    return GAETimetables(initstop=initialStopKey,
+                         inittime=timetable.initial[0],
                          table=gaetable)
 
 class TimetableItem(ndb.Model):
@@ -27,9 +34,15 @@ class TimetableItem(ndb.Model):
     stop = ndb.KeyProperty()
 
 class GAETimetables(polymodel.PolyModel):
-    inittime = ndb.DateTimeProperty()
+    inittime = ndb.TimeProperty()
     initstop = ndb.KeyProperty()
     table = ndb.StructuredProperty(TimetableItem, repeated=True)
     
     def toTimetable(self):
-        pass
+        initstop = self.initstop.get().toStop()
+        tt = mtt.Timetable(initStop=initstop, initTime=self.inittime)
+        for item in self.table:
+            s = item.stop.get().toStop()
+            t = datetime.timedelta(seconds=item.time) 
+            tt.insertItem(t, s)
+        return tt
